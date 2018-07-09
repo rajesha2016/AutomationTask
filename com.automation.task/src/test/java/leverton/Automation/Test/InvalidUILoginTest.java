3280eb4cf02f4fbc83bdfdfbfc80e675caa7980f
PROWARENESSDC\r.moharana <r.moharana@devonl.nl>
2018-07-09 19:18:59 +0530

    InvalidUILoginTest.java & InvalidUILoginPage.java

--- com.automation.task/src/test/java/leverton/Automation/Test/InvalidUILoginTest.java
+++ com.automation.task/src/test/java/leverton/Automation/Test/InvalidUILoginTest.java
@@ -1,7 +1,41 @@
 package leverton.Automation.Test;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import leverton.Automation.TestPage.InvalidUILoginPage;
+import leverton.automation.framework.TestDataProviderClass;
+import leverton.automation.framework.TestInitialization;
 
 
-public class InvalidUILoginTest {
+/**
+ * @author r.moharana
+ *
+ */
+public class InvalidUILoginTest extends TestInitialization{
+	
+	@Test(dataProvider="InvalidUILoginTestData",dataProviderClass=TestDataProviderClass.class)
+	public void testDataProvider(String _wrongUserName, String _wrongPassword,String _errorMessageToBeValidated) {
+		
+		
+		try {
+						
+				InvalidUILoginPage login_page=PageFactory.initElements(driver, InvalidUILoginPage.class);
+				
+				login_page.enterInvalidCredentials(_wrongUserName, _wrongPassword);
+				login_page.clickLoginButton();
+				
+				Assert.assertEquals(login_page.getErrorMessage(), _errorMessageToBeValidated,"Error message text validated successfully");
+				
+				System.out.println("*** Automated UI Test Completed for user name "+_wrongUserName+" & password "+_wrongPassword+" ***" );
+				
+			
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
 	
 }
--- com.automation.task/src/test/java/leverton/Automation/TestPage/InvalidUILoginPage.java
+++ com.automation.task/src/test/java/leverton/Automation/TestPage/InvalidUILoginPage.java
@@ -1,5 +1,38 @@
 package leverton.Automation.TestPage;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * @author r.moharana
+ *
+ */
 public class InvalidUILoginPage {
+	
+	@FindBy(xpath="//input[@id='username']")
+	WebElement userName;
+	
+	@FindBy(xpath="//input[@id='password-field']")
+	WebElement passWord;
+	
+	@FindBy(xpath="//input[@value='Log In']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//p[@class='subtitle']")
+	WebElement errorMessageText;
+	
+	public void enterInvalidCredentials(String uName, String pWord) {
+		userName.sendKeys(uName);
+		passWord.sendKeys(pWord);
+	}
+	
+	public void clickLoginButton() {
+		loginBtn.click();
+	}
+
+	public String getErrorMessage() {
+		
+		return errorMessageText.getText().trim();
+	}
 
 }
