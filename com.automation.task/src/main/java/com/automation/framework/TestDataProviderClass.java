947884d200d04defb78c20fd6d4880cd9b7e12f7
PROWARENESSDC\r.moharana <r.moharana@devonl.nl>
2018-09-03 16:35:53 +0530

    updated TestDataProviderClass.java file

--- com.automation.task/src/main/java/com/automation/framework/TestDataProviderClass.java
+++ com.automation.task/src/main/java/com/automation/framework/TestDataProviderClass.java
@@ -43,7 +43,8 @@ public class TestDataProviderClass {
             { "Germany"},
             { "India"},
             { "Australia"},
-            { "France" }
+            { "France" },
+            { "Italy" }
             
         };  
 	}
